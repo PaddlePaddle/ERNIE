11e3b35f234d366b73b7b9365e5d5d0c7c0d26e4
chenxuyi <chenxuyi@baidu.com>
2020-09-11 01:24:44 +0800

    use pathib, fix #546

--- ernie/file_utils.py
+++ ernie/file_utils.py
@@ -19,27 +19,44 @@ from __future__ import unicode_literals
 import os
 import logging
 from tqdm import tqdm
+from pathlib import Path
+import six
+if six.PY2:
+    from pathlib2 import Path
+else:
+    from pathlib import Path
 
 log = logging.getLogger(__name__)
 
-def _fetch_from_remote(url, force_download=False):
+def _fetch_from_remote(url, force_download=False, cached_dir='~/.paddle-ernie-cache'):
     import hashlib, tempfile, requests, tarfile
     sig = hashlib.md5(url.encode('utf8')).hexdigest()
-    cached_dir = os.path.join(tempfile.gettempdir(), sig)
-    if force_download or not os.path.exists(cached_dir):
-        with tempfile.NamedTemporaryFile() as f:
+    cached_dir = Path(cached_dir).expanduser()
+    try:
+        cached_dir.mkdir()
+    except OSError:
+        pass
+    cached_dir_model = cached_dir / sig
+    if force_download or not cached_dir_model.exists():
+        cached_dir_model.mkdir()
+        tmpfile = cached_dir_model / 'tmp'
+        with tmpfile.open('wb') as f:
             #url = 'https://ernie.bj.bcebos.com/ERNIE_stable.tgz'
             r = requests.get(url, stream=True)
             total_len = int(r.headers.get('content-length'))
-            for chunk in tqdm(r.iter_content(chunk_size=1024), total=total_len // 1024, desc='downloading %s' % url, unit='KB'):
+            for chunk in tqdm(r.iter_content(chunk_size=1024), 
+                    total=total_len // 1024, 
+                    desc='downloading %s' % url, 
+                    unit='KB'):
                 if chunk:
                     f.write(chunk)  
                     f.flush()
-            log.debug('extacting... to %s' % f.name)
-            with tarfile.open(f.name)  as tf:
-                tf.extractall(path=cached_dir)
+            log.debug('extacting... to %s' % tmpfile)
+            with tarfile.open(tmpfile.as_posix())  as tf:
+                tf.extractall(path=cached_dir_model.as_posix())
+        os.remove(tmpfile.as_posix())
     log.debug('%s cached in %s' % (url, cached_dir))
-    return cached_dir
+    return cached_dir_model
 
 
 def add_docstring(doc):
