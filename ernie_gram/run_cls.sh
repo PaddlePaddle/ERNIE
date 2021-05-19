33df1dc6832667ad333b76f354818013c002fe10
zhanghan17 <zhanghan17@baidu.com>
2021-05-20 01:11:38 +0800

    ernie-gram release demos

---
+++ ernie_gram/run_cls.sh
@@ -0,0 +1,13 @@
+source $1
+
+python3 -m paddle.distributed.launch ./ernie_gram/finetune_classifier_distributed.py \
+    --data_dir $data_dir \
+    --max_steps $max_steps \
+    --bsz $bsz \
+    --lr $lr \
+    --label_map ${label_map:-""} \
+    --num_labels $num_labels \
+    --pair_input $pair_input \
+    --valid_steps $valid_steps \
+    --from_pretrained $from_pretrained \
+    --save_dir checkpoints
---
+++ ernie_gram/run_ner.sh
@@ -0,0 +1,9 @@
+source $1
+
+python3 -m paddle.distributed.launch ./ernie_gram/finetune_ner.py \
+    --data_dir $data_dir \
+    --max_steps $max_steps \
+    --epoch $epoch \
+    --lr $lr \
+    --from_pretrained $from_pretrained \
+    --save_dir checkpoints
