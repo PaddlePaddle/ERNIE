b11c650ffb70efd5000cd22236239b9d70eb4cdd
SdeeRK <ganzehua@baidu.com>
2025-07-15 14:14:53 +0800

    fix 4d attn mask bug without flashmask

--- ernie/modeling_moe.py
+++ ernie/modeling_moe.py
@@ -1754,7 +1754,8 @@ class Ernie4_5_MoeForCausalLM(Ernie4_5_PretrainedModel):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
+        if attention_mask is not None and attention_mask.dtype != paddle.bool:
+            attention_mask = paddle.cast(attention_mask, paddle.bool)
         outputs = self.ernie(
             input_ids,
             position_ids=position_ids,
