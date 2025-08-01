4ef9f29764561c9b544a5a629cc05044782a51c5
YuangLiu <liuyuang@baidu.com>
2025-08-01 09:44:05 +0800

    bug fix

--- examples/pre-training/models/ernie/modeling_moe.py
+++ examples/pre-training/models/ernie/modeling_moe.py
@@ -150,7 +150,7 @@ def get_gate(
                 experts.append(None)
     assert (
         len(experts) == moe_num_experts
-    ), f"experts.len={len(experts)} != moe_num_experts_per_device={moe_num_experts}"
+    ), f"experts.len={len(experts)} != moe_num_experts={moe_num_experts}"
 
     logger.info(
         f"using moe-world-size: {config.moe_world_size} "
