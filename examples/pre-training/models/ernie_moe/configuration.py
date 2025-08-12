c3a9f33ee5247fb8e88229c0c02b7364f4a4e55f
xuexixi <xuexixi@baidu.com>
2025-08-12 19:54:50 +0800

    move ErnieConfig

--- examples/pre-training/models/ernie_moe/configuration.py
+++ examples/pre-training/models/ernie_moe/configuration.py
@@ -20,8 +20,7 @@ import json
 from typing import Union
 import paddle.distributed.communication.group
 
-from models.ernie.configuration import ErnieConfig
-
+from paddleformers.transformers.configuration_utils import PretrainedConfig
 logger = logging.getLogger(__name__)
 
 __all__ = [
@@ -49,6 +48,352 @@ ERNIE_PRETRAINED_INIT_CONFIGURATION = {
         "use_pure_fp16": False,
     },
 }
+
+
+class ErnieConfig(PretrainedConfig):
+    r"""
+    This is the configuration class to store the configuration of a [`~ErnieModel`]. It is used to instantiate an Ernie
+    model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
+    defaults will yield a similar configuration to that of the Ernie-7B.
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+    Args:
+        vocab_size (`int`, *optional*, defaults to 32000):
+            Vocabulary size of the Ernie model. Defines the number of different tokens that can be represented by the
+            `inputs_ids` passed when calling [`~ErnieModel`] or [`~TFErnieModel`].
+        hidden_size (`int`, *optional*, defaults to 4096):
+            Dimension of the hidden representations.
+        intermediate_size (`int`, *optional*, defaults to 11008):
+            Dimension of the MLP representations.
+        num_hidden_layers (`int`, *optional*, defaults to 32):
+            Number of hidden layers in the Transformer encoder.
+        num_attention_heads (`int`, *optional*, defaults to 32):
+            Number of attention heads for each attention layer in the Transformer encoder.
+        hidden_act (`str` or `function`, *optional*, defaults to `"silu"`):
+            The non-linear activation function (function or string) in the decoder.
+        initializer_range (`float`, *optional*, defaults to 0.02):
+            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+        rms_norm_eps (`float`, *optional*, defaults to 1e-12):
+            The epsilon used by the rms normalization layers.
+        use_cache (`bool`, *optional*, defaults to `True`):
+            Whether or not the model should return the last key/values attentions (not used by all models). Only
+            relevant if `config.is_decoder=True`.
+        tie_word_embeddings(`bool`, *optional*, defaults to `False`):
+            Whether to tie weight embeddings
+        Example:
+    ```python
+    >>> from paddleformers.transformer import ErnieModel, ErnieConfig
+
+    >>> # Initializing a Ernie ernie-7b style configuration
+    >>> configuration = ErnieConfig()
+
+    >>> # Initializing a model from the ernie-7b style configuration
+    >>> model = ErnieModel(configuration)
+
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    ```"""
+
+    model_type = "ernie"
+    attribute_map = {
+        "n_positions": "max_position_embeddings",
+        "n_embd": "hidden_size",
+        "n_layer": "num_hidden_layers",
+        "n_head": "num_attention_heads",
+        "n_inner": "intermediate_size",
+        "activation_function": "hidden_act",
+    }
+    pretrained_init_configuration = ERNIE_PRETRAINED_INIT_CONFIGURATION
+
+    def __init__(
+        self,
+        vocab_size=32000,
+        hidden_size=768,
+        intermediate_size=11008,
+        max_position_embeddings=32768,
+        num_hidden_layers=2,
+        num_attention_heads=2,
+        head_dim=None,
+        initializer_range=0.02,  # no use
+        rms_norm_eps=1e-6,
+        use_cache=False,
+        use_flash_attn=True,
+        use_mem_eff_attn=False,
+        use_flash_attn_with_mask=False,
+        use_recompute=False,
+        use_recompute_attn=False,
+        recompute_use_reentrant=False,
+        use_rmsnorm=True,
+        z_loss_lambda=None,
+        fuse_rms_norm=False,
+        fuse_ln=False,
+        pad_token_id=0,
+        bos_token_id=1,
+        eos_token_id=2,
+        fuse_attn_ffn=False,
+        fuse_swiglu=False,
+        use_bias=False,
+        expert_mlp_use_bias=None,
+        rope_reorder=True,
+        rope_theta=10000,
+        fuse_rope=False,
+        use_fast_ln=False,
+        weight_share_add_bias=True,
+        fuse_linear=False,
+        seqlen=False,  # pp only 由框架自动设置
+        ignored_index=-100,
+        remove_tail_layer=False,
+        use_recompute_lm_head=False,
+        use_recompute_loss_fn=False,
+        use_recompute_mtp=False,
+        use_recompute_dnd=False,
+        selective_no_recompute_num=0,
+        use_mp_gathered_weight=False,
+        refined_recompute=dict(),
+        attention_probs_dropout_prob=0.0,
+        hidden_dropout_prob=0.0,
+        compression_ratio: float = 1.0,
+        quant_bits=-1,  # For Inferencemodel, -1采用半精度推断
+        num_key_value_heads=None,
+        submatrix_parallel=False,
+        submatrix_parallel_low_memory=True,
+        use_sparse_head_and_loss_fn=False,
+        using_dynamic_sequence_length=False,
+        micro_batch_size=-1,
+        using_precision_check=False,
+        use_qk_norm=False,
+        use_tpsp_comm_overlap=False,
+        offload_pp_data_chunk_size=0,
+        use_fused_head_loss_fn=False,
+        use_recompute_resampler=False,
+        resampler_fuse_rms_norm=False,
+        token_loss_equal_weight=False,
+        token_balance_loss=False,
+        token_balance_seqlen=False,  # pretrain根据bsz与seqlen计算
+        use_fp8=False,
+        fp8_configs=dict(),
+        use_fp8_mlp=False,
+        fp8_mem_configs=dict(),
+        fp8_fused_ops_configs=dict(),
+        drop_before_deepep=False,
+        deepep_drop_padding=False,
+        disable_pipeline_warmup=False,
+        skip_align_position_id=False,
+        rope_3d=False,
+        freq_allocation=0,
+        moe_layer_feed_fake_token=False,
+        decoderlayer_act_offload_settings={"type": "", "value": ""},
+        loss_subbatch_seqlen=32768,
+        gate_force_zero_padding_grad=False,
+        recompute_num_layers=None,
+        use_combine_before_a2a=False,
+        use_quant_before_a2a=False,
+        rope_yarn_config={},
+        **kwargs,
+    ):
+        if "tie_word_embeddings" not in kwargs:
+            kwargs["tie_word_embeddings"] = False
+        super().__init__(
+            pad_token_id=pad_token_id,
+            bos_token_id=bos_token_id,
+            eos_token_id=eos_token_id,
+            **kwargs,
+        )
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.max_position_embeddings = max_position_embeddings
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.head_dim = head_dim
+        self.initializer_range = initializer_range
+        self.rms_norm_eps = rms_norm_eps
+        self.use_cache = use_cache
+        self.use_recompute_attn = use_recompute_attn
+        if use_recompute_attn:
+            logger.warning("set `use_recompute_attn`=True, disabling `use_recompute`")
+            use_recompute = False
+        self.use_recompute = use_recompute
+        self.recompute_num_layers = (
+            recompute_num_layers
+            if recompute_num_layers is not None
+            else num_hidden_layers
+        )
+        self.use_flash_attn = use_flash_attn
+        self.recompute_use_reentrant = recompute_use_reentrant
+        self.use_mem_eff_attn = use_mem_eff_attn
+        self.use_flash_attn_with_mask = use_flash_attn_with_mask
+        self.pad_token_id = pad_token_id
+        self.bos_token_id = bos_token_id
+        self.eos_token_id = eos_token_id
+        self.fuse_attn_ffn = fuse_attn_ffn
+        self.fuse_swiglu = fuse_swiglu
+        self.fuse_rms_norm = fuse_rms_norm
+        self.fuse_ln = fuse_ln
+        self.use_rmsnorm = use_rmsnorm
+        self.z_loss_lambda = z_loss_lambda
+        self.using_dynamic_sequence_length = using_dynamic_sequence_length
+        if using_dynamic_sequence_length:
+            assert (
+                micro_batch_size > 0
+            ), "micro_batch_size should be set when using_dynamic_sequence_length"
+        self.micro_batch_size = micro_batch_size
+        self.using_precision_check = using_precision_check
+        self.use_qk_norm = use_qk_norm
+
+        self.seqlen = seqlen
+        self.use_bias = use_bias
+        self.weight_share_add_bias = weight_share_add_bias
+        self.rope_reorder = rope_reorder  # 重新排列rope的顺序，防止间隔读取。
+        self.rope_yarn_config = rope_yarn_config
+        self.rope_theta = rope_theta
+        self.fuse_rope = fuse_rope
+        self.use_fast_ln = use_fast_ln
+
+        self.fuse_linear = fuse_linear
+        self.ignored_index = ignored_index
+        self.remove_tail_layer = remove_tail_layer
+        self.use_recompute_lm_head = use_recompute_lm_head
+        self.use_recompute_loss_fn = use_recompute_loss_fn
+        self.use_recompute_mtp = use_recompute_mtp
+        self.use_recompute_dnd = use_recompute_dnd
+
+        self.use_mp_gathered_weight = use_mp_gathered_weight
+        self.selective_no_recompute_num = selective_no_recompute_num  # only PP
+
+        self.refined_recompute = refined_recompute
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.compression_ratio = compression_ratio
+        """
+        `refined_recompute` 内容为一个dict:[op_name, skip_num], 目前只在PP模式下才生效。在PP中会根据`refined_recompute` 填充`self.skip_recompute_ops`
+            `op_name` 选择范围是："mlp_row_ln", "flash_attn", "attention_row_ln", "attention_column_ln", "mlp_column_ln"
+            `skip_num` 表示
+            选择不进行重计算的次数。
+            0表示 0次不重计算，也就是全部都重计算，显存最少。
+            -1表示全部不重计算，显存最多。
+            还可以填 【0，1，。。。，12】中的任意值，进行调整次数。
+            大于等于12。相当于 -1取值
+        """
+        self.skip_recompute_ops = dict()
+        self.quant_bits = quant_bits
+        self.num_key_value_heads = num_key_value_heads
+        self.submatrix_parallel = submatrix_parallel
+        self.submatrix_parallel_low_memory = submatrix_parallel_low_memory
+        self.use_sparse_head_and_loss_fn = use_sparse_head_and_loss_fn
+        self.use_tpsp_comm_overlap = use_tpsp_comm_overlap
+        self.offload_pp_data_chunk_size = offload_pp_data_chunk_size
+        self.use_fused_head_loss_fn = use_fused_head_loss_fn
+        self.use_recompute_resampler = use_recompute_resampler
+        self.resampler_fuse_rms_norm = resampler_fuse_rms_norm
+        self.token_balance_loss = token_balance_loss
+        self.token_balance_seqlen = token_balance_seqlen
+        self.rope_3d = rope_3d
+        self.freq_allocation = freq_allocation
+        self.decoderlayer_act_offload_settings = decoderlayer_act_offload_settings
+        self.loss_subbatch_seqlen = loss_subbatch_seqlen
+        self.gate_force_zero_padding_grad = gate_force_zero_padding_grad
+
+        # 默认的 fp8 设置
+        default_fp8_configs = {
+            "quant_scheme": "DelayedScaling",
+            "recipe": {
+                "format": "hybrid",
+                "calibrating": True,
+                "amax_history_len": 1024,
+                "amax_compute_algo": "max",
+                "fuse_wgrad_accumulation": False,
+                "quant_weight_at_first_microbatch": False,
+            },
+            "layers": {
+                "attn_fc1_linear": True,
+                "attn_fc2_linear": True,
+                "mlp_fc1_linear": True,
+                "mlp_fc2_linear": True,
+                "attn_tp_fc1_linear": True,
+                "attn_tp_fc2_linear": True,
+                "mlp_tp_fc1_linear": True,
+                "mlp_tp_fc2_linear": True,
+            },
+            "smooth_swiglu": False,
+        }
+
+        def update_nested_dict(default_dict, update_dict):
+            for key, value in update_dict.items():
+                if (
+                    isinstance(value, dict)
+                    and key in default_dict
+                    and isinstance(default_dict[key], dict)
+                ):
+                    update_nested_dict(default_dict[key], value)
+                else:
+                    default_dict[key] = value
+
+        # 更新默认设置
+        update_nested_dict(default_fp8_configs, fp8_configs)
+        self.fp8_configs = default_fp8_configs
+        self.use_fp8 = use_fp8
+        self.expert_mlp_use_bias = expert_mlp_use_bias
+        self.use_fp8_mlp = use_fp8_mlp
+        default_fp8_mem_configs = {
+            "shared_expert": False,
+            "recompute_fwd_gate_up": False,
+            "dequant_input": False,
+        }
+        update_nested_dict(default_fp8_mem_configs, fp8_mem_configs)
+        self.fp8_mem_configs = default_fp8_mem_configs
+        default_fp8_fused_ops_configs = {
+            "stack_quant": False,
+            "swiglu_probs_bwd": False,
+            "split_group_gemm": True,
+        }
+        update_nested_dict(default_fp8_fused_ops_configs, fp8_fused_ops_configs)
+        self.fp8_fused_ops_configs = default_fp8_fused_ops_configs
+        self.drop_before_deepep = drop_before_deepep
+        self.deepep_drop_padding = deepep_drop_padding
+        self.disable_pipeline_warmup = disable_pipeline_warmup
+        self.skip_align_position_id = skip_align_position_id
+        self.moe_layer_feed_fake_token = moe_layer_feed_fake_token
+
+        if self.sequence_parallel:
+            assert (
+                self.using_dynamic_sequence_length or self.seqlen
+            ), "seqlen not provided in sequence-parallel when not using dygramic sequence length"
+
+            assert (
+                self.tensor_parallel_degree > 1
+            ), f"senquence-parallel only works in mp, got mp={self.tensor_parallel_degree}"
+
+        self.register_nonsaveable_keys("use_recompute")
+        self.register_nonsaveable_keys("recompute_use_reentrant")
+        self.register_nonsaveable_keys("refined_recompute")
+        self.register_nonsaveable_keys("use_recompute_attn")
+        self.register_nonsaveable_keys("use_recompute_lm_head")
+        self.register_nonsaveable_keys("use_recompute_mtp")
+        self.register_nonsaveable_keys("use_recompute_dnd")
+        self.register_nonsaveable_keys("use_recompute_loss_fn")
+        self.register_nonsaveable_keys("using_precision_check")
+        self.register_nonsaveable_keys("skip_recompute_ops")
+
+    def __setattr__(self, name: str, value):
+        super().__setattr__(name, value)
+        if getattr(self, "use_recompute", False):
+            assert not getattr(
+                self, "use_recompute_attn", False
+            ), "cannot set `use_recompute_attn=True` when `use_recompute=True`"
+
+    def register_nonsaveable_keys(self, keys):
+        """
+        兼容PaddleNLP不同分支的API变更
+        """
+        if hasattr(super(), "register_nonsaveable_keys"):
+            return super().register_nonsaveable_keys(keys)
+        elif hasattr(super(), "register_unsavable_keys"):
+            return super().register_unsavable_keys(keys)
+        else:
+            raise AttributeError(
+                "register_nonsaveable_keys not found in PretrainedConfig"
+            )
 
 
 class ErnieMoEConfig(ErnieConfig):
--- examples/pre-training/models/moe/__init__.py
+++ examples/pre-training/models/moe/__init__.py
@@ -11,9 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# !/usr/bin/env python3
-"""
-MoE 模块，包含总控 `MOELayer` 以及各类 `Gate`
-"""
-from .moe_layer import MOELayer
-from .top2_gate import Top2Gate
