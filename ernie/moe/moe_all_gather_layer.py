49e161dd66626f8e6ff89b903f4fd68b55078a70
zjjlivein <1834792141@qq.com>
2025-09-02 19:25:44 +0800

    update

--- ernie/moe/moe_all_gather_layer.py
+++ ernie/moe/moe_all_gather_layer.py
@@ -367,9 +367,7 @@ class AlltoAllSmart(paddle.autograd.PyLayer):
                     .reshape([-1])
                 )
                 distributed_input_to_alltoall_out = paddle.maximum(
-                    (recv_mask_alltoall_out.cumsum() - 1).astype(
-                        recv_mask_alltoall_out.dtype
-                    ),
+                    recv_mask_alltoall_out.cumsum() - 1,
                     paddle.zeros([1], dtype=recv_mask_alltoall_out.dtype),
                 )
                 distributed_input_to_alltoall_out = (
@@ -394,7 +392,7 @@ class AlltoAllSmart(paddle.autograd.PyLayer):
 
                 recv_mask_alltoall_out = paddle.concat(recv_mask_alltoall_out, 0)
                 distributed_input_to_alltoall_out = paddle.maximum(
-                    recv_mask_alltoall_out.cumsum() - 1,
+                    (recv_mask_alltoall_out.cumsum() - 1).astype(recv_mask_alltoall_out.dtype),
                     paddle.zeros([1], dtype=recv_mask_alltoall_out.dtype),
                 )
                 distributed_input_to_alltoall_out = (
