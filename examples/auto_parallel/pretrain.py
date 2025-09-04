45164019b8a62c172e552aef7ca816248b665a8f
xuexixi <xuexixi@baidu.com>
2025-09-05 00:27:39 +0800

    fix

--- examples/auto_parallel/pretrain.py
+++ examples/auto_parallel/pretrain.py
@@ -56,19 +56,17 @@ from paddle.distributed import collective
 from paddle.tensor.manipulation import reshape
 from typing import Literal, TypeAlias
 
-use_intermediate_api = os.environ.get("USE_INTERMEDIATE_API", "0")
-if use_intermediate_api == "1":
+use_vpp = os.environ.get("USE_VPP", "0")
+if use_vpp == "1":
     from models.modeling import ErnieForCausalLM
 
     logger.info("Training with the intermediate API.")
-elif use_intermediate_api == "0":
+elif use_vpp == "0":
     from models.modeling_vpp import ErnieForCausalLM
 
     logger.info("Training VPP parallelism with the basic API")
 else:
-    raise ValueError(
-        f"Invalid environment args USE_INTERMEDIATE_API={use_intermediate_api}"
-    )
+    raise ValueError(f"Invalid environment args USE_VPP={use_vpp}")
 
 _ReduceMode: TypeAlias = Literal["mean", "sum", "none"]
 
--- examples/auto_parallel/train_4p5_300B_A47B.sh
+++ examples/auto_parallel/train_4p5_300B_A47B.sh
@@ -40,7 +40,7 @@ export PYTHONPATH=../../:$PYTHONPATH
 
 log_dir=output/paddle_distributed_logs
 
-export USE_INTERMEDIATE_API=1
+export USE_VPP=0
 
 python -m paddle.distributed.launch \
     --log_dir ${log_dir} \
