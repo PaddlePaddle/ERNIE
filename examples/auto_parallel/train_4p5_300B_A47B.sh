54d7492d9469e2ce3bf54047fc4dd993f3f33931
xuexixi <xuexixi@baidu.com>
2025-09-04 19:28:34 +0800

    add example

--- examples/auto_parallel/train_4p5_300B_A47B.sh
+++ examples/auto_parallel/train_4p5_300B_A47B.sh
@@ -44,6 +44,38 @@ python -m paddle.distributed.launch \
     --log_dir ${log_dir} \
     --master <master_ip>:<port> \
     --nnodes 7 \
+    --rank <current_rank> \
     --run_mode=collective \
     pretrain.py  \
     --config pretrain_4p5_300B_A47B.yaml
+
+# # Here is an example of the configuration and method for running scripts in a 7-machine cluster.
+# nnodes=7
+# START_RANK=0
+# END_RANK=$nnodes
+# # The current rank can be obtained through the preset environment variables in the cluster.
+# rank=$CURRENT_TRAINER_ID_FLAG
+
+# if [[ $rank -lt $START_RANK ]]; then
+#     exit 0
+# fi
+
+# if [[ $rank -ge $END_RANK ]]; then
+#     exit 0
+# fi
+# rank=$(($rank-$START_RANK))
+# # Typically, rank 0 is selected as the master of the cluster.
+# master=$RANK_0_IP
+# port=36677
+
+# python -m paddle.distributed.launch \
+#     --log_dir ${log_dir} \
+#     --master $master:$port \
+#     --nnodes $nnodes \
+#     --rank $rank \
+#     --run_mode=collective \
+#     pretrain.py  \
+#     --config pretrain_4p5_300B_A47B.yaml
+
+# # Running scripts in a cluster using the mpirun method.
+# mpirun bash train_4p5_300B_A47B.sh
