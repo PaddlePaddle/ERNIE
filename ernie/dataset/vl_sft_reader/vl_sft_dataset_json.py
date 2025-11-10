f842f09708cba3f11550fa4f14699ca922174577
forBlank <1422848728@qq.com>
2025-11-10 11:48:32 +0800

    update config for paddleocr_vl

--- ernie/dataset/vl_sft_reader/vl_sft_dataset_json.py
+++ ernie/dataset/vl_sft_reader/vl_sft_dataset_json.py
@@ -752,9 +752,6 @@ class SFTMultimodalDatasetJson(IterableDataset):
         indices = []
         for i, _ in enumerate(self.task_group):
             sample_size = int(self.weight_list[i] * self.length)
-            print(
-                f"Take {sample_size} samples from {self.task_group[i]._file_name} (total length: {len(self.task_group[i].exs)}) to construct current sample list"
-            )
             indices.extend([i] * sample_size)
         return indices
 
