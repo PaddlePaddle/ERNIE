6357e913103adc829846ead3bd4cef6f8b3a1c55
baiyue <liuyi39@baidu.com>
2025-12-01 10:33:16 +0800

    device = finetuning_args.device (#1379)
    * text_sft_reader默认设置GPU设备，XPU需要传入device参数。

--- erniekit/train/vl_sft/workflow.py
+++ erniekit/train/vl_sft/workflow.py
@@ -700,6 +700,7 @@ def run_vl_sft(
                 "use_train_part_sharding": finetuning_args.text_use_train_part_sharding,
                 "rope_3d": model_args.rope_3d,
                 "chat_template": preprocess_args.chat_template,
+                "device": finetuning_args.device,
             }
 
             text_sft_train_reader = create_pyreader(config_dataset_text)
